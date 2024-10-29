772bed13ba726619a4a429d540cf6ea3473749c3
toshikatsu1201 <hitofuta1201@icloud.com>
2024-10-29 15:14:13 +0900

    itemマイグレーションファイル修正

--- db/migrate/20241028085608_create_items.rb
+++ db/migrate/20241028085608_create_items.rb
@@ -7,6 +7,12 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.string :title, null: false
       t.references :user, null: false, foreign_key: true
 
+      t.integer :condition_id, null:false
+      t.integer :cost_id, null:false
+      t.integer :category_id, null:false
+      t.integer :region_id, null:false
+      t.integer :schedule_day_id, null:false
+
       t.timestamps
     end
   end
