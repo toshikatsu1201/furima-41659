719c81d327a5dd54b5fff02050470f989a3a477d
toshikatsu1201 <hitofuta1201@icloud.com>
2024-10-29 18:15:54 +0900

    テストコード完了

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,11 @@
 window.addEventListener('turbo:load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById('item-price');
+  const feeOutput = document.getElementById('add-tax-price');
+  const profitOutput = document.getElementById('profit');
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    feeOutput.innerHTML = Math.floor(inputValue * 0.1);  // 10%の手数料
+    profitOutput.innerHTML = Math.floor(inputValue * 0.9);  // 利益を計算して表示エリアに反映する（価格の90%を計算し、小数点以下は切り捨て）
+ })
 });
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
+# spec/factories/items.rb
 FactoryBot.define do
   factory :item do
-    
+    title { "テスト商品" }
+    explanation { "テスト商品の説明" }
+    price { 5000 }
+    condition_id { 1 } # 使用する状態のID
+    cost_id { 1 }      # 配送料の負担のID
+    category_id { 1 }  # カテゴリーのID
+    region_id { 1 }    # 発送元の地域のID
+    schedule_day_id { 1 } # 発送までの日数のID
+
+    association :user # ユーザーとのアソシエーションを追加
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png' )
+    end
   end
 end
