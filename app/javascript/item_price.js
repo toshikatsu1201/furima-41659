c4b75ac0c7147fc28ad5b3b774976fcd4c431ea0
toshikatsu1201 <hitofuta1201@icloud.com>
2024-10-29 19:44:11 +0900

    JavaScript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,16 @@
-window.addEventListener('turbo:load', () => {
+function calculateProfit() {
   const priceInput = document.getElementById('item-price');
   const feeOutput = document.getElementById('add-tax-price');
   const profitOutput = document.getElementById('profit');
 
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    feeOutput.innerHTML = Math.floor(inputValue * 0.1);  // 10%の手数料
-    profitOutput.innerHTML = Math.floor(inputValue * 0.9);  // 利益を計算して表示エリアに反映する（価格の90%を計算し、小数点以下は切り捨て）
- })
-});
+  if (priceInput) { // priceInputが存在する場合のみイベントリスナーを設定
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      feeOutput.innerHTML = Math.floor(inputValue * 0.1);  // 10%の手数料
+      profitOutput.innerHTML = Math.floor(inputValue * 0.9);  // 利益を計算して表示エリアに反映する（価格の90%を計算し、小数点以下は切り捨て）
+    });
+  }
+}
+
+window.addEventListener('turbo:load', calculateProfit);
+window.addEventListener('turbo:render', calculateProfit);
